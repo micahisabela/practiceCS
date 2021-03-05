5c570d2f718a6bf6a1fc1f2807b7acb64fec44ff
Micah Kelly <micahisabela@gmail.com>
2021-03-04 21:26:49 -0800

    completed challenges 1 and 2

---
+++ Passcode/Passcode/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Passcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var passcode = "secret";
+            var prompt = "What is your password?";
+
+            Console.WriteLine(prompt);
+            var userInput = Console.ReadLine();
+
+            if(userInput == passcode)
+            {
+                Console.WriteLine("Logged in!");
+            }
+            else
+            {
+                Console.WriteLine("Try again!");
+            }
+        }
+    }
+}
