78d43b44fbc28a1ae7a3159924fe2c4f31542b9a
Micah Kelly <micahisabela@gmail.com>
2021-03-11 23:16:50 -0800

    worked on debugging and passing parameters using Pluralsight course

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -4,9 +4,20 @@ namespace GradeBook
 {
     class Program
     {
+        // this is a method. the name of this method is Main
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // goal: pass my name as a parameter
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine("Hello");
+            }
         }
+        // end of the method
     }
 }
