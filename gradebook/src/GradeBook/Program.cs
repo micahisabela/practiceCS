4448e9a665179fcf02802cc14d3ecc29abab4242
Micah Kelly <micahisabela@gmail.com>
2021-03-13 15:03:34 -0800

    started learning how to do arithmetic using C#

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -7,6 +7,13 @@ namespace GradeBook
         // this is a method. the name of this method is Main
         static void Main(string[] args)
         {
+            var grades = new double[3];
+            grades[0] = 12.7;
+            grades[1] = 34.5;
+            grades[2] = 77.9;
+
+            Console.WriteLine(grades[0] + grades[1] + grades[2]);
+
             // goal: pass my name as a parameter
 
             if (args.Length > 0)
