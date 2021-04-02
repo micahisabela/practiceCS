7020ea20572774cfd9406efb6b3b6b96ad622d85
Micah Kelly <micahisabela@gmail.com>
2021-04-01 22:56:11 -0700

    added comments to the .cs file

--- gradebook/test/GradeBook.Tests/UnitTest1.cs
+++ gradebook/test/GradeBook.Tests/UnitTest1.cs
@@ -1,11 +1,11 @@
 using System;
-using Xunit;
+using Xunit; // contains types and APIs to use when using xunit
 
 namespace GradeBook.Tests
 {
     public class UnitTest1
     {
-        [Fact]
+        [Fact] // this is an attribute - a piece of data that is attached the symbol that follows it. in this case, it is attached to Test1 (below)
         public void Test1()
         {
 
