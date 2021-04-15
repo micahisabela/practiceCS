73f34e3c75c62107bbe2e7d1b07e7e744221da3a
Micah Kelly <micah@Augmentoids-MBP.augbio.lan>
2021-04-14 17:31:31 -0700

    added notes to the unit test file

--- gradebook/test/GradeBook.Tests/UnitTest1.cs
+++ gradebook/test/GradeBook.Tests/UnitTest1.cs
@@ -8,12 +8,15 @@ namespace GradeBook.Tests
         [Fact] // this is an attribute - a piece of data that is attached the symbol that follows it. in this case, it is attached to Test1 (below)
         public void Test1()
         {
+            // arrange:
             var x = 5;
             var y = 2;
             var expected = 7;
 
-            var actual = x * y;
+            // act
+            var actual = x + y;
             
+            // assert 
             Assert.Equal(expected, actual);
         }
     }
